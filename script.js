bb4491df50b15437ba06c344e637417a8b268f71
Myoungju Cha <101177202+MyoungjuReyna@users.noreply.github.com>
2023-05-09 21:59:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -35,26 +35,38 @@ function draw() {
     var ctx = canvas.getContext('2d');
     // Black
     ctx.fillStyle = 'rgb(0, 0, 0)'; 
-    ctx.fillRect(borderBarx, 0, 60, pageHeight);
-    ctx.fillRect( 0 , borderBary, pageWidth, 50 );
-    ctx.fillRect(0, 300 , borderBary, 80); 
-    // Red
-    ctx.fillStyle = 'rgb(255, 0, 0)'; 
-    ctx.fillRect(borderBarx+60, 0, pageWidth - borderBarx, borderBary);
-    // Blue
-    ctx.fillStyle = 'rgb(0, 0, 255)'; 
-    ctx.fillRect( 0 ,  borderBary+50, borderBarx, borderBary + pageHeight );
-     // Black
-     ctx.fillStyle = 'rgb(0, 0, 0)'; 
-     ctx.fillRect(1800, borderBary, 60, borderBary + pageHeight );
-     // Black
-     ctx.fillStyle = 'rgb(0, 0, 0)'; 
-     ctx.fillRect(1800, borderBary + 300, pageWidth-borderBary, 60 );
-    // Yellow
-    // https://www.rapidtables.com/web/color/RGB_Color.html
-    ctx.fillStyle = 'rgb(255, 255, 0)'; 
-    ctx.fillRect( 1860 ,  borderBary + 360, borderBarx, borderBary + pageHeight );
+    ctx.fillRect(borderBarx - 100, 0, 30, pageWidth);
+    ctx.fillRect(0 , borderBary, pageWidth, 30 );
 
+    // TOPLEFT
+    ctx.fillStyle = 'rgb(255, 0, 0)'
+    ctx.fillRect(0, 0, 500, 800)
+
+    // Blue
+    ctx.fillStyle = 'rgb(51, 51, 255)'; 
+    ctx.fillRect( 1300 ,  borderBary+25, borderBarx, 300);
+     // Black
+     ctx.fillStyle = 'rgb(0, 0, 0)'; 
+     ctx.fillRect(borderBarx + 700, borderBary, 30, borderBary + pageHeight );
+     // Black
+     ctx.fillStyle = 'rgb(0, 0, 0)'; 
+     ctx.fillRect(1300, borderBary + 300, pageWidth-borderBary, 40 );
+    
+     // Yellow
+    ctx.fillStyle = 'rgb(255, 255, 0)'; 
+    ctx.fillRect( borderBarx - 70,  borderBary + 700, borderBary -400, 150);
+
+    // Gray Box
+    ctx.fillStyle = 'rgb(160, 160, 160)'; 
+    ctx.fillRect( borderBarx + 300,  borderBary + 700, borderBary -400, 150);
+
+    // Black
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(borderBarx - 80, pageHeight -20 , borderBary, 30);
+    // Black
+    ctx.fillStyle = 'rgb(0, 0, 0)'; 
+    ctx.fillRect(900, borderBary +700, 30, borderBary + pageHeight );
+    
     }
   }
 
